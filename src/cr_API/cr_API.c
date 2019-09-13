f27c98f76820369c7777614c3a46fe1d04d86907
Daniel Leal <dlleal@uc.cl>
2019-09-13 20:13:33 -0300

    Initial commit

---
+++ src/cr_API/cr_API.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../constants/constants.h"
+#include "cr_API.h"
+
+
+// File structs
+struct crfile {
+
+};
+
+
+// File management functions
+crFILE *cr_open(char *path, char *mode)
+{
+    crFILE *file_desc = malloc(sizeof(crFILE));
+    return file_desc;
+}
+
+
+int cr_read(crFILE *file_desc, void *buffer, int nbytes)
+{
+    return 0;
+}
+
+
+int cr_write(crFILE *file_desc, void *buffer, int nbytes)
+{
+    return 0;
+}
+
+
+int cr_close(crFILE *file_desc)
+{
+    free(file_desc);
+    return 0;
+}
+
+
+int cr_rm(char *path)
+{
+    return 0;
+}
+
+
+int cr_unload(char *orig, char *dest)
+{
+    return 0;
+}
+
+
+int cr_load(char *orig)
+{
+    return 0;
+}
+
+
+// General API functions
+void cr_mount(char *diskname)
+{
+
+}
+
+
+void cr_bitmap(unsigned block, bool hex)
+{
+
+}
+
+
+int cr_exists(char *path)
+{
+    return 0;
+}
+
+
+void cr_ls(char *path)
+{
+
+}
+
+
+int cr_mkdir(char *foldername)
+{
+    return 0;
+}
---
+++ src/cr_API/cr_API.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+// File structs
+typedef struct crfile crFILE;
+
+// File management functions
+crFILE *cr_open(char *path, char *mode);
+int cr_read(crFILE *file_desc, void *buffer, int nbytes);
+int cr_write(crFILE *file_desc, void *buffer, int nbytes);
+int cr_close(crFILE *file_desc);
+int cr_rm(char *path);
+int cr_unload(char *orig, char *dest);
+int cr_load(char *orig);
+
+// General API functions
+void cr_mount(char *diskname);
+void cr_bitmap(unsigned block, bool hex);
+int cr_exists(char *path);
+void cr_ls(char *path);
+int cr_mkdir(char *foldername);
