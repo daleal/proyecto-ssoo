dfd8e46606ea7e83d82536f8577829af485d5fe3
Cschavez <cschavez@uc.cl>
2019-10-07 12:40:35 -0300

    main listo

--- src/crfs/main.c
+++ src/crfs/main.c
@@ -9,16 +9,16 @@ int main(int argc, char **argv)
     cr_bitmap(1, false);
 
     //Lee intro.txt y guarda en buffer 
-    crFile *file_r = cr_open("intro.txt", 'r');
+    crFile *file_r = cr_open("/intro.txt", 'r');
     unsigned int size = file_r->index->size;
     unsigned char buffer[size];
     cr_read(file_r, buffer, size);
     cr_close(file_r);
 
     //Escribe en intro_copy el mismo contenido de intro.txt
-    // crFILE *file_w = cr_open("/memes/intro_copy.txt", 'w');
-    // cr_write(file_w, buffer, size); 
-    // cr_close(file_W);
+    crFILE *file_w = cr_open("/memes/intro_copy.txt", 'w');
+    cr_write(file_w, buffer, size); 
+    cr_close(file_W);
 
     //Eee en dos partes el archivo para mostrar funcionalidad
     //de reader
@@ -33,11 +33,11 @@ int main(int argc, char **argv)
 
     cr_close(file_incomplete);
     
-    // cr_rm("/memes/intro_copy.txt");
+    cr_rm("/memes/intro_copy.txt");
 
-    cr_unload("/Withered Leaf.mp3", "/NO SE QUE DESTINO PONER");
+    cr_unload("/Withered Leaf.mp3", "./");
 
-    //cr_load("pikachu2.png"); //esto deberia estar en nuestro pc o no?
+    cr_load("/pikachu2.png"); 
 
     cr_unmount();
     return 0;
